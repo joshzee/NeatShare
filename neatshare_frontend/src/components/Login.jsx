5e95d8e081c1805eb08dae895af75f71bbae32dd
Josh Zarazovski <joshuazarazovski@gmail.com>
2023-03-05 19:55:44 +1100

    Tab Change

--- neatshare_frontend/src/components/Login.jsx
+++ neatshare_frontend/src/components/Login.jsx
@@ -7,29 +7,29 @@ import logoWhite from '../assets/logowhite.png';
 
 const Login = () => {
   return (
-  	<div className="flex justify-start items-center flex-col h-screen">
+    <div className="flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
-				<video
-					src={shareVideo}
-					type="video/mp4"
-					loop
-					controls={false}
-					muted
-					autoPlay
-					className="w-full h-full object-cover"
-				/>
+        <video
+          src={shareVideo}
+          type="video/mp4"
+          loop
+          controls={false}
+          muted
+          autoPlay
+          className="w-full h-full object-cover"
+        />
       </div>
 
-			<div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
-				<div className="p-5">
-					<img src={logoWhite} width="130px" alt="logo" />
-				</div>
+      <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+        <div className="p-5">
+          <img src={logoWhite} width="130px" alt="logo" />
+        </div>
 
 
       <div className="App">
           <GoogleLogin
             onSuccess={credentialResponse => {
-							var decoded = jwt_decode(credentialResponse.credential);
+              var decoded = jwt_decode(credentialResponse.credential);
               console.log(decoded);
             }}
           
@@ -39,9 +39,9 @@ const Login = () => {
           
           />
       </div>
-		</div>
-	</div>
-	)
+    </div>
+  </div>
+  )
 }
 
 export default Login
