54afa0fefab91ee84e5c7235f573e7682e871407
Josh Zarazovski <joshuazarazovski@gmail.com>
2023-03-05 19:51:55 +1100

    Setup Google Sign in with @react-oauth/google

--- neatshare_frontend/src/App.jsx
+++ neatshare_frontend/src/App.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Routes, Route, useNavigate } from 'react-router-dom'
+import Login from './components/Login'
+import Home from './container/Home'
+import { GoogleLogin } from '@react-oauth/google';
+
+
 
 const App = () => {
+  
   return (
-    <h1 className="text-3xl font-bold underline">
-      NeatSHARE
-    </h1>
+    <Routes>
+      <Route path="login" element={<Login />} />
+      <Route path="/*" element={<Home />} />
+    </Routes>
   )
 }
 
--- neatshare_frontend/src/main.jsx
+++ neatshare_frontend/src/main.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter as Router } from 'react-router-dom'
+import { GoogleOAuthProvider } from '@react-oauth/google'
+
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Router>
+    <GoogleOAuthProvider 
+    clientId={import.meta.env.VITE_GOOGLE_AUTH_CLIENTID}>
+      <App />
+      </GoogleOAuthProvider>
+    </Router>
   </React.StrictMode>,
 )
