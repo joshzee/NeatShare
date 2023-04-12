076ab6a6a35b346c6ba580bf7bbdbc56e36370cd
Josh Zarazovski <joshuazarazovski@gmail.com>
2023-04-12 17:23:15 +1000

    Fix small error in Login component for build

--- neatshare_frontend/src/components/Login.jsx
+++ neatshare_frontend/src/components/Login.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client'
-import { GoogleLogin } from '@react-oauth/google';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
-import shareVideo from '../assets/login.mp4';
-import logoNs from '../assets/logo_ns.png';
+import shareVideo from "../assets/login.mp4";
+import logoNs from "../assets/logo_ns.png";
 
-import { client } from '../client';
-import { useNavigate } from 'react-router-dom';
-
+import { client } from "../client";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -16,35 +15,31 @@ const Login = () => {
 
     console.log(decode);
 
-    try{
-      localStorage.setItem('user', JSON.stringify(decode));
+    try {
+      localStorage.setItem("user", JSON.stringify(decode));
 
       const { name, picture, sub } = decode;
       console.log(decode);
 
       const doc = {
         _id: sub,
-        _type: 'user',
+        _type: "user",
         userName: name,
-        image: picture
+        image: picture,
       };
-  
-      client.createIfNotExists(doc)
-      .then(() => {
-      navigate('/', {replace: true})
 
-      })
-      .catch(error => console.log(error))
-    } 
-    catch (e) {
-      localStorage.clear()
-      console.log(e)
-    // End try  
+      client
+        .createIfNotExists(doc)
+        .then(() => {
+          navigate("/", { replace: true });
+        })
+        .catch((error) => console.log(error));
+    } catch (e) {
+      localStorage.clear();
+      console.log(e);
+      // End try
     }
-  }
-
-
-
+  };
 
   return (
     <div className="flex justify-start items-center flex-col h-screen">
@@ -57,7 +52,7 @@ const Login = () => {
           muted
           autoPlay
           className="w-full h-full object-cover"
-        /> 
+        />
       </div>
 
       <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-black/50">
@@ -66,16 +61,16 @@ const Login = () => {
         </div>
 
         <div className="shadow-2x1">
-        <GoogleLogin
-            clientId='{`${import.meta.env.VITE_GOOGLE_AUTH_CLIENTID}`}'
+          <GoogleLogin
+            clientId={`${import.meta.env.VITE_GOOGLE_AUTH_CLIENTID}`}
             onSuccess={responseGoogle}
             onError={responseGoogle}
             cookiePolicy="single_host_origin"
           />
+        </div>
       </div>
     </div>
-  </div> 
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
